b025b10bef6994549b7fef15dd712c6464bb3ac3
AceCapitalEnterprise <acecapitalent@gmail.com>
2024-09-23 09:46:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from breeze_connect import BreezeConnect
 import urllib
 breeze = BreezeConnect(api_key="77%U3I71634^099gN232777%316Q~v4=")
 breeze.generate_session(api_secret="9331K77(I8_52JG2K73$5438q95772j@",
-                        session_token="47363196")
+                        session_token="47437327")
 
 
 import numpy as np
